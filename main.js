58f4620a0321072565a5ef97caea6eb37d766f5c
TeteyChuy <marco.antonio.lopezjal@gmail.com>
2022-08-05 14:44:11 -0500

    Updates

--- main.js
+++ main.js
@@ -24,16 +24,16 @@ async function fetchData(urlApi){
       <div class="group relative">
         <div
           class="w-full bg-green-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full" >
         </div>
-        <div class="mt-4 flex justify-between">
+        <div class="mt-4 flex justify-between" >
           <h3 class="text-lg text-green-700">
           <span aria-hidden="true" class="absolute inset-0"></span>
           ${video.snippet.title} <br>
           </h3>
         </div>
         <div class="mt-4 flex justify-between">
-          <h4 class="text-lg text-#1438D7-700">
+          <h4 class="text-lg text-black">
           ${video.snippet.description}
           </h4>
           </div>
