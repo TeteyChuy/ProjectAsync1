3c97918bcd6425ce8ebcb385ef23b0482ac50222
TeteyChuy <marco.antonio.lopezjal@gmail.com>
2022-08-05 13:43:16 -0500

    Updates

--- main.js
+++ main.js
@@ -23,16 +23,21 @@ async function fetchData(urlApi){
       `${videos.items.map(video=>`      
       <div class="group relative">
         <div
-          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          class="w-full bg-green-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
           <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
-          <h3 class="text-sm text-gray-700">
+          <h3 class="text-lg text-green-700">
           <span aria-hidden="true" class="absolute inset-0"></span>
-          ${video.snippet.title}
+          ${video.snippet.title} <br>
           </h3>
         </div>
-      </div> `).slice(0,4).join('')}
+        <div class="mt-4 flex justify-between">
+          <h4 class="text-lg text-#1438D7-700">
+          ${video.snippet.description}
+          </h4>
+          </div>
+      </div> `).slice(2,10).join('')}
       `; 
 content.innerHTML = view;
     }
